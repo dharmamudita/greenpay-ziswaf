4e6e45544165ff08982fee56bd2a92004bafbce3
dharma mudita <dharmamudita404@gmail.com>
2026-07-10 02:13:43 +0700

    memperbarui alamat ip server di layanan socket io agar sesuai dengan jaringan lokal

--- mobile/services/socket.js
+++ mobile/services/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 // Sesuaikan URL ini dengan alamat IP komputer Anda yang menjalankan backend
 // (Sama dengan IP yang ada di api.js tanpa tambahan /api)
-const SOCKET_URL = 'http://192.168.1.100:5000';
+const SOCKET_URL = 'http://192.168.1.13:5000';
 
 const socket = io(SOCKET_URL, {
   autoConnect: false, // Jangan otomatis connect dulu, tunggu login
